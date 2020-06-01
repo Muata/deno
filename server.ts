614f36c8cf40aeb5522beaa6e4a0340eec468044
Igor <muata77@gmail.com>
2020-06-01 10:37:05 +0000

    hello world

---
+++ server.ts
@@ -0,0 +1,6 @@
+import { serve } from "https://deno.land/std/http/server.ts";
+const body = "Hello World\n";
+const server = serve({ port: 8000 });
+for await (const req of server) {
+  req.respond({ body });
+}
