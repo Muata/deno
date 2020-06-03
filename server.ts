7c23ad5cf42e37c7fb8b1f4fcdb72f02d9e9b323
Igor <muata77@gmail.com>
2020-06-03 13:49:48 +0000

    update

--- server.ts
+++ server.ts
@@ -1,27 +1,9 @@
-import {
-  createApp,
-  createRouter,
-  serveStatic,
-} from "https://servestjs.org/@v1.1.0/mod.ts";
-import {
-  cors,
-} from "https://servestjs.org/@v1.1.0/middleware/cors.ts";
+import { createApp, serveStatic } from "https://servestjs.org/@v1.1.0/mod.ts";
 const app = createApp();
-app.use(
-  // allow access to static resource only from (*.)servestjs.org
-  cors({
-    origin: /twitch\.tv$/,
-    methods: ["GET", "HEAD"],
-  }),
-  serveStatic("public"),
-);
-const www = createRouter();
-// allow all access to /api routes
-www.use(cors({
-  origin: "*",
-  methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["x-my-api-token"],
-  maxAge: 300,
-}));
-app.route("/public", www);
+// All requests will be processed and matched files in "public" directory
+// are served automatically
+// Otherwise, request will be passed to next handler
+app.use(serveStatic("./public"));
 app.listen({ port: 8000 });
+
+
