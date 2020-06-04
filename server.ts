1f57dd31c222f5c45e0e9565eb523dd53bf01bda
Igor <muata77@gmail.com>
2020-06-04 16:36:03 +0000

    just for sure

--- server.ts
+++ server.ts
@@ -1,9 +1,18 @@
-import { createApp, serveStatic } from "https://servestjs.org/@v1.1.0/mod.ts";
+import {
+	createApp,
+	createRouter,
+	serveStatic,
+} from "https://servestjs.org/@v1.1.0/mod.ts";
+import {
+	cors,
+} from "https://servestjs.org/@v1.1.0/middleware/cors.ts";
 const app = createApp();
-// All requests will be processed and matched files in "public" directory
-// are served automatically
-// Otherwise, request will be passed to next handler
-app.use(serveStatic("./public"));
+app.use(
+	// allow access to static resource only from (*.)servestjs.org
+	cors({
+		origin: "*",
+		methods: ["GET", "HEAD", "POST", "PUT", "PATCH", "DELETE"],
+	}),
+	serveStatic("public"),
+);
 app.listen({ port: 8000 });
-
-
