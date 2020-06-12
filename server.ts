b21bea195273b628e63a40db8560f95212ca4413
Igor <muata77@gmail.com>
2020-06-12 09:03:23 +0000

    up

--- server.ts
+++ server.ts
@@ -1,7 +1,10 @@
 import { createApp, serveStatic } from "https://servestjs.org/@v1.1.0/mod.ts";
+import { cors } from "https://servestjs.org/@v1.1.0/middleware/cors.ts";
 const app = createApp();
 // All requests will be processed and matched files in "public" directory
 // are served automatically
 // Otherwise, request will be passed to next handler
-app.use(serveStatic("./public"));
+
+app.use(
+serveStatic("./public"));
 app.listen({ port: 8000 });
